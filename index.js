2e146bd4380f001c909a88efa547473985221da1
Francisco <francsicogajardo.perez@gmail.com>
2023-01-29 19:59:33 -0300

    save in mongo

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-const MONGO_URL = process.env.MONGO_URL;
+// env
 const MONGOPASSWORD = process.env.MONGOPASSWORD;
 const MONGOUSER = process.env.MONGOUSER;
 const MONGOHOST = process.env.MONGOHOST;
-const {MongoClient} = require('mongodb');
 
+// express and mongoose 
 const express = require("express");
 const bodyParse =  require("body-parser");
-const uri = `mongodb+srv://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}/test?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
-
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
+ 
 
+// apps 
 app.use(bodyParse.urlencoded({extended: true}));
 app.use(bodyParse.json());
 
-app.get("/", (req,res) => {
-    res.status(200).send("listo papurro")
+// config and connection bd 
+mongoose.set("strictQuery", false);
+mongoose.connect(`mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}`, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
 });
 
-app.get("/saludo",(req , res) => {
-    res.status(200).send("hola");
-})
+// entity 
+const personaBd = {
+    nombre: String,
+    apellido: String,
+ };
 
-app.post("/adios",(req,res) => {
-    const { nombre } = req.body;
-    res.status(200).send(`adios ${nombre}`)
-})
+ // model and entity
+const Persona = mongoose.model("persona", personaBd);
+
+
 
 app.listen(PORT, () =>{
     console.log(`tu puerto esta listo en ${PORT}`)
 })
 
-async function connect(){
- 
-    try {
-        await client.connect();
-        console.log("conectado con exito");
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-connect().catch(console.error);
 
+app.get("/", (req,res) => {
+    res.status(200).send("listo papurro")
+});
+
+app.post("/save", function (req, res) {
+    const persona = new Persona({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+    });
+    persona.save(function (err) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send("save")
+        }
+    });
+ });
+
+ 
+app.post("/adios",(req,res) => {
+    const { nombre } = req.body;
+    res.status(200).send(`adios ${nombre}`)
+})
 
  
