9eae8ea0d76e08ea96eab32aa84f64f3d92005e8
Francisco <francsicogajardo.perez@gmail.com>
2023-01-29 18:15:59 -0300

    primer test

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParse =  require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParse.urlencoded({extended: true}));
+app.use(bodyParse.json());
+
+app.get("/", (req,res) => {
+    res.status(200).send("listo papurro")
+});
+
+app.get("saludo",(req , res) => {
+    res.status(200).send("hola");
+})
+
+app.post("adios",(req,res) => {
+    const { nombre } = req.body;
+    res.status(200).send(`adios ${nombre}`)
+})
+
+app.listen(PORT, () =>{
+    console.log(`tu puerto esta listo en ${PORT}`)
+})
