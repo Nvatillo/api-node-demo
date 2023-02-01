b37b119e2d309b92d014fb228bdf484ff898c8e1
Francisco <francsicogajardo.perez@gmail.com>
2023-02-01 00:08:57 -0300

    cors

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const express = require("express");
 const bodyParse =  require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
- 
+const cors = require("cors");
+
+
 // apps 
 app.use(bodyParse.urlencoded({extended: true}));
 app.use(bodyParse.json());
 app.use("/" , require('./route/router'));
-
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200
+  }));
 
 app.listen(PORT, () =>{
     console.log(`tu puerto esta listo en ${PORT}`)
